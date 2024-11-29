8d6be87fc17bcdfedf9db94abc2296eb0ae2dbb4
Sayanmondal13m <sm1555524@gmail.com>
2024-11-29 22:51:33 +0530

    Updated features

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
-import withPWA from 'next-pwa';
-
 /** @type {import('next').NextConfig} */
-const nextConfig = withPWA({
+const nextConfig = {
   reactStrictMode: true,
-  pwa: {
-    dest: 'public', // The folder where the service worker and assets are stored
-    register: true, // Automatically register the service worker
-    skipWaiting: true, // Activate the new service worker as soon as it's available
-  },
-});
+};
 
 export default nextConfig;
