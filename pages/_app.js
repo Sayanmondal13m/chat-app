7fde6a2826d6dfd0e7651fd303e63101cf570d14
Sayanmondal13m <sm1555524@gmail.com>
2024-11-30 11:45:45 +0530

    Add PWA support

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
-import "@/styles/globals.css";
+import { useEffect } from 'react';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js').then(
+        (registration) => {
+          console.log('Service Worker registered with scope:', registration.scope);
+        },
+        (err) => {
+          console.error('Service Worker registration failed:', err);
+        }
+      );
+    }
+  }, []);
+
   return <Component {...pageProps} />;
 }
+
+export default MyApp;
