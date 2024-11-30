0047f5e6bd58b31b720d69c7af767836df193ae8
Sayanmondal13m <sm1555524@gmail.com>
2024-11-30 11:26:48 +0530

    Updated features

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+/**
+ *  @type {import('next').NextConfig} 
+ */
+import withpwa from "next-pwa";
 
-export default nextConfig;
+const NextConfig = {
+  distdir: "build",
+  reactstrictmode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development", 
+  } 
+}
+
+export default withpwa({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+})(NextConfig);
